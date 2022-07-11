86a35860c82e7b3d28c0f59d07d35e8a2ce50f99
Krasikov.VA <vital.krasikov@gmail.com>
2022-07-11 08:29:55 +0300

    Telegram bot

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # news-parser
+
+News parser is meant to get the fresh news from rbc.ru by parsing this web-site. News are shown by the means of Telegram bot @rbcnews1518195631bot
+
+Functionality of the bot includes the /news command, which shows the list of the latest news and the set of numerical commands /1, /2 etc., each allowing to get the detailed information on the corresponding item.
--- parsing.py
+++ parsing.py
@@ -7,25 +7,37 @@ class NewsParser:
     def __init__(self):
         self.news = []
 
-    def parse(self):
-        url = 'https://www.rbc.ru/short_news'
+    def parse(self, url='https://www.rbc.ru/short_news'):
 
         req = requests.get(url)
 
         soup = BeautifulSoup(req.text, 'html.parser')
 
-        tags = soup.find_all("div", "item__wrap l-col-center")
-        self.news = []
+        if url == 'https://www.rbc.ru/short_news':
+            self.news = []
 
-        for tag in tags:
-            title = tag.find("span", "item__title rm-cm-item-text").text
-            heading = tag.find("a", "item__category").text.replace(",", "")
-            if title is not None:
-                if heading is not None:
-                    heading = heading.strip()
-                item = {'title': title.strip(), 'time': tag.find("span", "item__category").text,
-                        'heading': heading}
-            self.news.append(item)
+            tags = soup.find_all("div", "item__wrap l-col-center")
+
+            for tag in tags:
+                title = tag.find("span", "item__title rm-cm-item-text").text
+                heading = tag.find("a", "item__category").text.replace(",", "")
+                link = tag.find("a", "item__link")['href']
+                if title is not None:
+                    if heading is not None:
+                        heading = heading.strip()
+                    item = {'title': title.strip(), 'time': tag.find("span", "item__category").text,
+                            'heading': heading, 'link': link}
+                self.news.append(item)
+        else:
+            tags = soup.find_all("p")
+            text = ''
+            for tag in tags:
+                if tag.text != '' and tag.text[0] != '\n':
+                    text += tag.text + '\n'
+            if len(text) > 4096:
+                return text[:4093] + '...'
+            else:
+                return text
 
     def save(self):
         f = open("digest.txt", "w")
@@ -37,5 +49,11 @@ class NewsParser:
         for item in self.news:
             print(item['title'] + ", " + item['time'] + ", " + item['heading'])
 
+    def get_news(self):
+        return self.news
+
+    def get_detailed(self, index):
+        url = self.news[index]['link']
+        return self.parse(url)
 
 
